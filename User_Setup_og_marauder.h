93d268c95bf672fac226c5859f6ea468bf22cfa4
AdamK2003 <60717729+AdamK2003@users.noreply.github.com>
2023-11-04 19:55:00 +0100

    disable rst pin, collision with miso

--- User_Setup_og_marauder.h
+++ User_Setup_og_marauder.h
@@ -179,12 +179,12 @@
 */
 
 // ESP32 Marauder
-#define TFT_MISO 12
+// #define TFT_MISO 12
 #define TFT_MOSI 13
 #define TFT_SCLK 14
 #define TFT_CS 15  // Chip select control pin
 #define TFT_DC 2   // Data Command control pin
-#define TFT_RST 12 // Reset pin (could connect to RST pin)
+#define TFT_RST -1 // Reset pin (could connect to RST pin)
 //#define TFT_RST  -1  // Set TFT_RST to -1 if display RESET is connected to
 // ESP32 board RST
 
